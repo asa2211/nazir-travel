3e93b568d22b0cbc29f6cbcc8c6ccd5c0922e02f
Abdulazizov Sarvar Abduhamid o'g'li <139462360+asa2211@users.noreply.github.com>
2025-02-21 05:30:57 +0000

    analitik

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './assets/styles/main.scss'
 import { BrowserRouter } from 'react-router-dom'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics/>
       <SpeedInsights/>
     </BrowserRouter>
   </React.StrictMode>,
